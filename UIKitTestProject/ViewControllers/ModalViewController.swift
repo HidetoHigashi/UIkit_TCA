b2c68a0fd162d3c94118cc5172f7b4b17156b3b9
HidetoHigashi <hideto.higashi@glpgs.com>
2022-11-07 16:47:26 +0900

    ラベルの追加、ラベルの制約追加

--- UIKitTestProject/ViewControllers/ModalViewController.swift
+++ UIKitTestProject/ViewControllers/ModalViewController.swift
@@ -9,11 +9,32 @@ import ComposableArchitecture
 import UIKit
 
 class ModalViewController: UIViewController {
-
+    
+    private var modalLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.text = "モーダル"
+        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.view.backgroundColor = .white
+
+        self.view.addSubview(modalLabel)
+        
+        applyConstraints()
+    }
+    
+    private func applyConstraints() {
+        let modalLabelConstraints = [
+            modalLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            modalLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+        ]
+        
+        NSLayoutConstraint.activate(modalLabelConstraints)
     }
 }
 
